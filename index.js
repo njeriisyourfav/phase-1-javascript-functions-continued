dfeaa0d4a4735731daf6694fda20e09328dde128
lilian njeri ndungu <lilian.ndungu@student.moringaschool.com>
2022-08-23 09:25:30 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // code your solution here
+//function saturdayFun
+function saturdayFun(activity="roller-skate") {
+  return `This Saturday, I want to ${activity}!`
+}
+//function mondayWork
+let mondayWork = function(activity="go to the office") {
+  return `This Monday, I will ${activity}.`
+}
+//function wrapAdjective
+let wrapAdjective = function(activity="*") {
+  return function(adjective="special") {
+    return `You are ${activity}${adjective}${activity}!`
+  }
+}
